801d9290a6d46d7ca40b7b5938ac6c8ef0ba2a25
Anna Maria <aannamariabr@outlook.com>
2022-06-02 11:57:12 -0300

    Adicionando imagens e novas rotas

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from portfolio.views import hello
+from . import views
+
+app_name = 'portfolio'
 
 urlpatterns = [
-    path('', hello),
+    path('', views.hello, name="hello"),
+    path('about_me/', views.about_me, name="about_me"),
+    path('skills/', views.skills, name="skills"),
+    path('experience/', views.experience, name="experience"),
+    path('contact/', views.contact, name="contact"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def hello(request):
     return render(request, 'portfolio/pages/hello.html')
+
+
+def about_me(request):
+    return render(request, 'portfolio/pages/about_me.html')
+
+
+def skills(request):
+    return render(request, 'portfolio/pages/skills.html')
+
+
+def experience(request):
+    return render(request, 'portfolio/pages/experience.html')
+
+
+def contact(request):
+    return render(request, 'portfolio/pages/contact.html')
